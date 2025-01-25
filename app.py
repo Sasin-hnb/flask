cd6641d926bf6bb902ed69954788e7c729ae8ec2
Sasin-hnb <AvyaanDavid@outlook.com>
2025-01-26 01:43:12 +0300

    edit app.py

--- app.py
+++ app.py
@@ -951,5 +951,5 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host="0.0.0.0")
+    # app.run(debug=True)
+    app.run(host="0.0.0.0")
